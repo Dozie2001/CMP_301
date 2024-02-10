115f73394ba4be5b2d36980ab0d5423fd103b007
Dozie2001 <davidchidozie402@gmail.com>
2024-02-10 22:48:56 +0100

    New file

---
+++ second.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int a =10; // variable declaration
+//check the boolean condition
+if (a < 20)
+{
+// if condition is true then print the following
+cout << "a is less than 20; " << endl;
+}
+cout << "value of a is : " << a << endl;
+
+
+    return 0;
+}
---
+++ third.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+
+    cout << "Put in your favorite drink" << endl;
+    cin >> choice;
+    if (choice == 1) {
+        cout << "Coke\n"
+    }
+
+}
