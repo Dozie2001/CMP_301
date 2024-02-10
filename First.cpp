8f44d321d22c6f8e44a562df822c6c936fb54b7e
Dozie2001 <davidchidozie402@gmail.com>
2024-02-10 16:24:21 +0100

    New file

---
+++ First.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Header file for input and output operations
+using namespace std; // Using the standard namespace
+
+int main() {
+    cout << "Hello World!"; // Printing "Hello World!" to the console
+    return 0; // Indicating successful program execution
+}
